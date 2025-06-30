57f5e8f886ae774cd806c4ae0ba1308e53a105f1
akash <akash9122000@gmail.com>
2025-06-30 17:49:39 +0530

    add meta data in api instance

--- product/main.py
+++ product/main.py
@@ -10,7 +10,23 @@ from .database import engine, SessionLocal
 from typing import List
 from passlib.context import CryptContext
 
-app = FastAPI()
+app = FastAPI(
+    title='Products API',
+    description='Get details for all the products and seller on website',
+    terms_of_service='http://www.google.com',
+    contact={
+        'Develoer name': "Akash",
+        'website': 'http://www.google.com',
+        'email': 'admin@abc.com',
+    },
+    license_info={
+        'name': 'License',
+        'url': 'http://www.google.com'
+    },
+    # docs_url='/documentation',
+    # redoc_url=None
+
+)
 
 models.Base.metadata.create_all(engine)
 
@@ -25,7 +41,7 @@ def get_db():
         db.close()
 
 
-@app.put('/product/{id}')
+@app.put('/product/{id}', tags=['Products'])
 def update_product(id: int, request: schemas.Product, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id)
     if product.first():
@@ -35,14 +51,14 @@ def update_product(id: int, request: schemas.Product, db: Session = Depends(get_
     return {'error': 'Product not found'}
 
 
-@app.delete('/produc/{id}')
+@app.delete('/produc/{id}', tags=['Products'])
 def del_product(id, db: Session = Depends(get_db)):
     db.query(models.Product).filter(models.Product.id == id).delete(synchronize_session=False)
     db.commit()
     return {f"product deleted with id: {id}"}
 
 
-@app.get('/product/{id}', response_model=schemas.DisplayProduct)
+@app.get('/product/{id}', response_model=schemas.DisplayProduct, tags=['Products'])
 def products(id, response: Response, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id).first()
     if not product:
@@ -50,13 +66,13 @@ def products(id, response: Response, db: Session = Depends(get_db)):
     return product
 
 
-@app.get('/products', response_model=List[schemas.DisplayProduct])
+@app.get('/products', response_model=List[schemas.DisplayProduct], tags=['Products'])
 def products(db: Session = Depends(get_db)):
     products = db.query(models.Product).all()
     return products
 
 
-@app.post('/product')
+@app.post('/product', tags=['Products'])
 def add(request: schemas.Product, db: Session = Depends(get_db)):
     new_product = models.Product(name=request.name, description=request.description, price=request.price, seller_id=1)
     db.add(new_product)
@@ -65,7 +81,7 @@ def add(request: schemas.Product, db: Session = Depends(get_db)):
     return request
 
 
-@app.post('/seller', response_model=schemas.DisplaySeller)
+@app.post('/seller', response_model=schemas.DisplaySeller, tags=['Seller'])
 def create_seller(request: schemas.Seller, db: Session = Depends(get_db)):
     hashed_password = pwd_context.hash(request.password)
     new_seller = models.Seller(username=request.username, email=request.email, password=hashed_password)
